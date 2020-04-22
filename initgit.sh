98c9e7054aec80e6da5b34080b4ab812e6527ab4
Mike Møller Nielsen <mmni@protonmail.com>
2020-04-23 00:14:19 +0200

    files

---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# springboot-scheduling-threads" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ekim197711/springboot-scheduling-threads.git
+git push -u origin master
